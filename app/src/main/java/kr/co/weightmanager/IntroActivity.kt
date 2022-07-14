04418b61393663a100961e8855d4c879efc4f3c4
organguy <organguy@naver.com>
2022-07-15 02:50:50 +0900

    - 구글 인증 연동 완료

---
+++ app/src/main/java/kr/co/weightmanager/IntroActivity.kt
@@ -0,0 +1,92 @@
+package kr.co.weightmanager
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import kr.co.weightmanager.databinding.ActivityIntroBinding
+import kr.co.weightmanager.databinding.ActivityMainBinding
+
+class IntroActivity : AppCompatActivity() {
+
+
+    lateinit var binding : ActivityIntroBinding;
+
+    private lateinit var getResult: ActivityResultLauncher<Intent>
+    private lateinit var client: GoogleSignInClient
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityIntroBinding.inflate(layoutInflater);
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        if(auth.currentUser != null){
+            gotoMain()
+        }else{
+            //setResultListener();
+            initGoogleLogin()
+        }
+    }
+
+    private fun initGoogleLogin(){
+        val options = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail().build()
+
+        client = GoogleSignIn.getClient(this, options)
+
+        getResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+
+            val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+            var account: GoogleSignInAccount? = null
+            try {
+                account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account!!.idToken)
+            } catch (e: ApiException) {
+
+                Toast.makeText(this, e.localizedMessage, Toast.LENGTH_SHORT).show()
+
+                //Toast.makeText(this, "Failed Google Login", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.loginButton.setOnClickListener {
+            // 로그인 요청
+            //startActivityForResult(client.signInIntent, 1)
+
+            getResult.launch(client.signInIntent)
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String?) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this,
+                OnCompleteListener<AuthResult?> { task ->
+                    if (task.isSuccessful) {
+                        // 인증에 성공한 후, 현재 로그인된 유저의 정보를 가져올 수 있습니다.
+                        gotoMain()
+
+                    }
+                })
+    }
+
+    private fun gotoMain(){
+        Toast.makeText(this, "gotoMain", Toast.LENGTH_SHORT).show()
+    }
+}
